cb5c26e1f3a3eacfc36cad77cd1056ccc54ff4ca
jbgross <gross.joshua.b@gmail.com>
2014-05-04 13:43:15 -0400

    new files for class

---
+++ numbers-practice.rkt
@@ -0,0 +1,7 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname numbers-practice) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp") (lib "batch-io.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp") (lib "batch-io.rkt" "teachpack" "2htdp")))))
+(/ 4 6)
+(define x 3)
+(define y 4)
+(string-append "what a " "lovely " "day" " for learning BSL")
